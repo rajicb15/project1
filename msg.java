f1dfb0ba1599d98cbe4687fc6044b2237b4dd696
rajicb15 <rajikommireddy15@gmail.com>
2026-03-09 23:35:17 +0530

    Update msg.java

--- msg.java
+++ msg.java
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
-int main() {
-    int num;
-    printf("Enter a number: ");
-    scanf("%d", &num);
-    printf("You entered: %d\n", num);
-    return 0;
+{
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Hello, Raji");
+    }
 }
 
